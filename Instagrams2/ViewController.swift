5d4b5c4802bcb1fcb6e777e0bdd5c88e641d0074
thirukumaran19 <thirukumaran@twilightsoftwares.com>
2021-08-31 18:56:28 +0530

    initial Commit

---
+++ Instagrams2/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Instagra
+//
+//  Created by SaranRaj on 30/08/21.
+//  Copyright © 2021 Twilight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
